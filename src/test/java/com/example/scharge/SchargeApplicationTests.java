20da1e0e693a875b9f0d7828aa6cc3f9eacd8cf5
mirliancakalli <mirliancakalli@gmail.com>
2024-02-21 12:06:19 +0100

    base code
    base code

---
+++ src/test/java/com/example/scharge/SchargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
